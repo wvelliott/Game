1fe31a0ddaeb9a6dadf7e3165790e6729cf36405
William Elliott <wvelliott@email.wm.edu>
2020-05-26 16:38:33 -0400

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by William Elliott on 5/26/20.
+//  Copyright © 2020 wvelliott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
